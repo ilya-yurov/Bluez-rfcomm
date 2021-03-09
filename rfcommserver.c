b2efd3bf79f832bdfed9a68ea0bf00326a3f93cb
ilya-yurov <76982614+ilya-yurov@users.noreply.github.com>
2021-03-09 13:49:50 +0300

    Some comments have been added

--- rfcommserver.c
+++ rfcommserver.c
@@ -12,26 +12,26 @@ int main(int argc, char **argv)
     socklen_t opt = sizeof(rem_addr);
 
     // выделяем сокет
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM); //системный вызов socket основывается на информации о коммуникационном домене и типе сокета s = socket(domain, type, protocol)
 
     // привязываем сокет к порту 1 первого доступного локального адаптера bluetooth
-    loc_addr.rc_family = AF_BLUETOOTH; //семейство адресов сокета и всегда будет AF_BLUETOOTH
+    loc_addr.rc_family = AF_BLUETOOTH; //семейство адресов сокета AF_BLUETOOTH
     loc_addr.rc_bdaddr = *BDADDR_ANY; //любой локальный адаптер Bluetooth является приемлемым
     loc_addr.rc_channel = (uint8_t) 1; //указывается номер порта для прослушивания
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr)); //Для связывания сокета с адресом и номером порта используют системный вызов bind. bind(s, name, namelen);
 
     // переводим сокет в режим прослушивания
-    listen(s, 1);
-
+    listen(s, 1); //системный вызов listen связывает сокет с общеизвестным адресом, ассоциирующимся с данным сервисом, и пассивно слушает этот сокет.
+                  //listen(s, qlength) где s это дескриптор сокета, а qlength это максимальное количество запросов на установление связи, которые могут стоять в очереди, ожидая обработки сервером
     // принимаем одно соединение
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+    client = accept(s, (struct sockaddr *)&rem_addr, &opt); //системный вызов accept создает новый сокет и связывает его с ассоциацией, эквивалентной 'слушающему сокету'
 
-    ba2str( &rem_addr.rc_bdaddr, buf );
+    ba2str( &rem_addr.rc_bdaddr, buf ); //ba2str распаковывает  6-байтовый bdaddr_t и выдает строку вида «XX: XX: XX: XX: XX: XX», где каждый XX является шестнадцатеричным числом, определяющим октет 48-битного адреса
     fprintf(stderr, "accepted connection from %s\n", buf);
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, sizeof(buf)); //заполняем массив нулями
 
     // читаем данные от клиента
-    bytes_read = read(client, buf, sizeof(buf));
+    bytes_read = read(client, buf, sizeof(buf)); //читаем полученные от клиента данные при помощи системного вызова read(s, buf, sizeof(buf))
     if( bytes_read > 0 ) {
         printf("received [%s]\n", buf);
     }
